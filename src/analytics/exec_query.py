1c5839e3937b0450334f37b2a5f570aea1abdc25
Marcello Miranda <marcello.usa963@gmail.com>
2025-11-14 20:29:34 -0300

    Salvando tudo pra main

--- src/analytics/exec_query.py
+++ src/analytics/exec_query.py
@@ -3,6 +3,7 @@ import pandas as pd
 import sqlalchemy
 import datetime
 from tqdm import tqdm
+import argparse
 
 # %%
 # Importando query
@@ -12,17 +13,8 @@ def import_query(path):
     
     return query
 
-query = import_query("life_cycle.sql")
-print(query)
-# %%
-# Criando conexao com os database
-engine_app = sqlalchemy.create_engine("sqlite:///../../data/loyalty-system/database.db")
-
-engine_analytics = sqlalchemy.create_engine("sqlite:///../../data/analytics/database.db")
-
 # %%
 # INGESTAO DE DADOS 
-
 
 def ingest_dates(start,stop):
     dates = []
@@ -30,27 +22,52 @@ def ingest_dates(start,stop):
         dates.append(start)
         dt_start = datetime.datetime.strptime(start,"%Y-%m-%d") + datetime.timedelta(days=1)
         start = datetime.datetime.strftime(dt_start,"%Y-%m-%d")
+        
     return dates 
 
-dates = ingest_dates("2025-03-01","2025-09-01")
+def exec_query(dt_start,dt_stop,table,db_origin,db_target):
 
+    # Criando conexao com os database
+    engine_app = sqlalchemy.create_engine(f"sqlite:///../../data/{db_origin}/database.db")
+    engine_analytics = sqlalchemy.create_engine(f"sqlite:///../../data/{db_target}/database.db")
 
-for i in tqdm(dates):
+    query = import_query(f"{table}.sql")
+    dates = ingest_dates(dt_start,dt_stop)
 
-    with engine_analytics.connect() as con:
-        try:
-            con.execute(sqlalchemy.text(f"DELETE FROM life_cycle WHERE dtRef = date('{i}','-1 day')"))
-            con.commit()
+    for i in tqdm(dates):
 
-        except Exception as e:
-            print(e)
+        with engine_analytics.connect() as con:
+            try:
+                con.execute(sqlalchemy.text(f"DELETE FROM {table} WHERE dtRef = date('{i}','-1 day')"))
+                con.commit()
 
-    print(i)
-    format_query = query.format(date=i)
-    df = pd.read_sql(format_query,engine_app)
-    df.to_sql("life_cycle",engine_analytics,index=False,if_exists='append')
+            except Exception as e:
+                print(e)
+
+        print(i)
+        format_query = query.format(date=i)
+        df = pd.read_sql(format_query,engine_app)
+        df.to_sql(f"{table}",engine_analytics,index=False,if_exists='append')
     
-
 # %%
 
+def main():
+
+    parser = argparse.ArgumentParser()
+    stop = datetime.datetime.now().strftime("%Y-%M-%d")
+
+    parser.add_argument("--start",type=str,default="2024-03-01")
+    parser.add_argument("--stop",type=str,default=stop)
+    parser.add_argument("--table",type=str,help="Tabela que vai ser processada com o mesmo nome do arquivo")
+    parser.add_argument("--db_origin",choices=['loyalty-system','education-plataform'],default='loyalty-system')
+    parser.add_argument("--db_target",choices=['analytics'],default='analytics')
+
+    args = parser.parse_args()
+    
+    exec_query(args.start,args.stop,args.table,args.db_origin,args.db_target)
+
+
+
+if __name__ == '__main__':
+    main()
 
