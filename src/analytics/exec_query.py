15e62eb63bbae5155075247e66e8a84deeb23463
Marcello Miranda <marcello.usa963@gmail.com>
2025-11-14 22:28:49 -0300

    Automatizando minha ingestao de dados

--- src/analytics/exec_query.py
+++ src/analytics/exec_query.py
@@ -16,23 +16,25 @@ def import_query(path):
 # %%
 # INGESTAO DE DADOS 
 
-def ingest_dates(start,stop):
+def ingest_dates(start,stop,monthly = False):
     dates = []
     while start <= stop :
         dates.append(start)
         dt_start = datetime.datetime.strptime(start,"%Y-%m-%d") + datetime.timedelta(days=1)
         start = datetime.datetime.strftime(dt_start,"%Y-%m-%d")
-        
+    if monthly:
+        return [i for i in dates if i.endswith("01")]
+
     return dates 
 
-def exec_query(dt_start,dt_stop,table,db_origin,db_target):
+def exec_query(dt_start,dt_stop,table,db_origin,db_target,monthly):
 
     # Criando conexao com os database
     engine_app = sqlalchemy.create_engine(f"sqlite:///../../data/{db_origin}/database.db")
     engine_analytics = sqlalchemy.create_engine(f"sqlite:///../../data/{db_target}/database.db")
 
     query = import_query(f"{table}.sql")
-    dates = ingest_dates(dt_start,dt_stop)
+    dates = ingest_dates(dt_start,dt_stop,monthly)
 
     for i in tqdm(dates):
 
@@ -54,17 +56,18 @@ def exec_query(dt_start,dt_stop,table,db_origin,db_target):
 def main():
 
     parser = argparse.ArgumentParser()
-    stop = datetime.datetime.now().strftime("%Y-%M-%d")
+    now = datetime.datetime.now().strftime("%Y-%M-%d")
 
-    parser.add_argument("--start",type=str,default="2024-03-01")
-    parser.add_argument("--stop",type=str,default=stop)
+    parser.add_argument("--start",type=str,default=now)
+    parser.add_argument("--stop",type=str,default=now)
+    parser.add_argument("--monthly",action='store_true')
     parser.add_argument("--table",type=str,help="Tabela que vai ser processada com o mesmo nome do arquivo")
-    parser.add_argument("--db_origin",choices=['loyalty-system','education-plataform'],default='loyalty-system')
+    parser.add_argument("--db_origin",choices=['loyalty-system','education-plataform','analytics'],default='loyalty-system')
     parser.add_argument("--db_target",choices=['analytics'],default='analytics')
 
     args = parser.parse_args()
     
-    exec_query(args.start,args.stop,args.table,args.db_origin,args.db_target)
+    exec_query(args.start,args.stop,args.table,args.db_origin,args.db_target,args.monthly)
 
 
 
